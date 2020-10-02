8dd6d455ad9235adb0096672b18ff0824cf313dd
Imari P <imaridp23@gmail.com>
2020-10-01 21:57:44 -0400

    Adding prompts and conditions for code selection

--- script.js
+++ script.js
@@ -14,7 +14,30 @@ var confirmLowerCase;
 var confirmUpperCase;
 var confirmSpecialChar;
 
-// Write password to the #password input
+// Prompt for user to confirm length of password
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like to include in your password? Choose between 8 and 25."));
+
+  // Condition if user does not enter a value
+  if (!confirmLength) {
+      alert("You must enter a value!");
+  // Condition if user chooses a value outside of the parameters provided
+  } else if (confirmLength <= 7 || confirmLength >= 26) {
+      alert("Your password must be between 8 and 25 characters!");
+  // Repeat length input back to user
+  } else {
+      alert(`Your password will contain ${confirmLength} characters.`);
+  }
+  
+  // Continuation of prompts once input is validated
+  var confirmNumbers = confirm("Do you want this password to contain numbers?")
+  var confirmLowerCase = confirm("Do you want this password to contain lower case letters?")
+  var confirmUpperCase = confirm("Do you want this password to contain upper case letters?")
+  var confirmSpecialChar = confirm("Do you want this password to contain special characters?")
+  
+}
+
+// Write password to the #password input 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
